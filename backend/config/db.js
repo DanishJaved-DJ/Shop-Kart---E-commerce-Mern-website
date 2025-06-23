ba731fcd651b63a907981a80f850f6a1a736908d
DanishJaved-DJ <danishjaved03191@gmail.com>
2025-06-23 23:50:37 +0530

    fix: simplify mongoose connection by removing unused options

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,14 +6,11 @@ const mongoose_URL= process.env.MONGOOSE_URL;
 
 const dataBaseConnect=async ()=>{
    await mongoose
-    .connect(mongoose_URL,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    .connect(mongoose_URL)
     .then((conn)=>console.log(`connected to mongoDB: ${conn.connection.host}`))
-    .catch((err)=>console.log(err.message)
-    )
-    
+    .catch((err)=>console.log(`Error connecting to MongoDB: ${err.message}`)
+    );
+
 }
 
 export default dataBaseConnect;
