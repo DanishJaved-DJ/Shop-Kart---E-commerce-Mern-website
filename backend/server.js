9097c572270e69856720389d5d22681b4fef0245
DanishJaved-DJ <danishjaved03191@gmail.com>
2025-06-23 16:11:27 +0530

    refactor: clean up server.js by removing unused imports and static file serving code

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import path from "path";
-import { fileURLToPath } from "url";
-import express from "express"; // ⬅️ This is required here to use `app.use`
+
+import express from "express";
 
 dotenv.config();
 
@@ -10,17 +10,6 @@ import dataBaseConnect from './config/db.js';
 
 const PORT = process.env.PORT || 8562;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// ✅ Serve Vite static files
-app.use(express.static(path.join(__dirname, "../client/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-});
-
-// ✅ Start server after DB connection
 dataBaseConnect().then(() => {
   app.listen(PORT, () => {
     console.log(`🚀 Server running at http://localhost:${PORT}`);
